2b15558d966a9b5a645d28441d9695d786cd6dee
CarlosRuiz <ingecarlosrm@gmail.com>
2024-03-14 13:40:44 -0600

    [UPDATE] estructura

--- main.js
+++ main.js
@@ -1,18 +1,11 @@
-// script.js
-let progressBar = document.getElementById('progress-bar');
-let targetWidth = 90; // Queremos llegar al 50%
-let duration = 3000; // Duración deseada de la animación hasta el 50%: 5 segundos
-let interval = 190; // Intervalo de actualización en milisegundos
-let increment = (targetWidth / duration) * interval; // Calculamos el incremento por intervalo
-let width = 0;
-
-function updateProgressBar() {
-    width += increment;
-    if (width >= targetWidth) {
-        width = increment; // Reinicia justo después de alcanzar el 50%, manteniendo el flujo continuo
-        progressBar.style.width = '0%'; // Reinicia visualmente la barra
-    }
-    progressBar.style.width = width + '%';
-}
-
-setInterval(updateProgressBar, interval);
+document.addEventListener('DOMContentLoaded', function() {
+    var animationContainer = document.getElementById('lottie-animation');
+    var animData = {
+        container: animationContainer, // ID del contenedor donde se reproducirá la animación
+        renderer: 'svg',
+        loop: true,
+        autoplay: true,
+        path: '/json/proximamente.json' // La ruta al archivo JSON de tu animación
+    };
+    var anim = lottie.loadAnimation(animData);
+});

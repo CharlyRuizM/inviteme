43c64a25dc2f0c22c7d5dd531b216b33140602b5
Carlos Ruiz <54676097+CharlyRuizM@users.noreply.github.com>
2024-05-01 21:46:50 -0600

    [UPDATE] lottie

--- main.js
+++ main.js
@@ -1,11 +1,24 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var animationContainer = document.getElementById('lottie-animation');
+    loadLottieAnimation('full-width-lottie', './json/pt1.json');
+    loadLottieAnimation('lottie1', './json/pt2.json');
+    loadLottieAnimation('lottie2', './json/pt3.json');
+    loadLottieAnimation('lottie3', './json/pt4.json');
+    loadLottieAnimation('lottie4', './json/pt5.json');
+    loadLottieAnimation('lottie5', './json/pt6.json');
+    loadLottieAnimation('lottie6', './json/pt7.json');
+    loadLottieAnimation('lottie7', './json/pt8.json');
+    loadLottieAnimation('lottie8', './json/pt9.json');
+    loadLottieAnimation('lottie9', './json/pt10.json');
+});
+
+function loadLottieAnimation(containerId, path) {
+    var animationContainer = document.getElementById(containerId);
     var animData = {
         container: animationContainer, // ID del contenedor donde se reproducirá la animación
         renderer: 'svg',
         loop: true,
         autoplay: true,
-        path: './json/proximamente.json' // La ruta al archivo JSON de tu animación
+        path: path // La ruta al archivo JSON de tu animación
     };
     var anim = lottie.loadAnimation(animData);
-});
+}

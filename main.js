3cd4df196555ece964775aea31ea34cfe095e828
Carlos Ruiz <54676097+CharlyRuizM@users.noreply.github.com>
2024-05-01 22:22:52 -0600

    [UPDATE] loader

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
+    loadLottieAnimation('full-width-lottie-loader', './json/proximamente.json');
+    loadLottieAnimation('full-width-lottie-intro', './json/Intro.json');
     loadLottieAnimation('full-width-lottie', './json/pt1.json');
     loadLottieAnimation('lottie1', './json/pt2.json');
     loadLottieAnimation('lottie2', './json/pt3.json');
@@ -22,3 +24,38 @@ function loadLottieAnimation(containerId, path) {
     };
     var anim = lottie.loadAnimation(animData);
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    var loader = document.getElementById('full-width-lottie-loader');
+    var intro = document.getElementById('full-width-lottie-intro');
+    var mainContent = document.getElementById('main-content');
+
+    // Función para ocultar el loader y mostrar el intro
+    function hideLoader() {
+        loader.style.opacity = 0;
+        setTimeout(function() {
+            loader.style.display = 'none';
+            intro.style.display = 'block';
+            setTimeout(function() {
+                intro.style.opacity = 1;
+            }, 10);
+        }, 500); // Coincide con la duración de la transición de opacidad
+    }
+
+    // Simular carga y ocultar loader después de un tiempo fijo o puedes ajustarlo para cuando realmente termine de cargar todo
+    window.onload = function() {
+        hideLoader();
+    };
+
+    intro.addEventListener('click', function() {
+        // Ocultar el intro y mostrar el contenido principal
+        intro.style.opacity = 0;
+        setTimeout(function() {
+            intro.style.display = 'none';
+            mainContent.style.display = 'block';
+            setTimeout(function() {
+                mainContent.style.opacity = 1;
+            }, 10);
+        }, 500);
+    });
+});

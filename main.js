3e37e3ec8fb1f259bd9c23b4c7811dfa09da072c
Carlos Ruiz <54676097+CharlyRuizM@users.noreply.github.com>
2024-05-01 22:42:28 -0600

    [UPDATE] loader adjust

--- main.js
+++ main.js
@@ -1,9 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-    loadLottieAnimation('full-width-lottie-loader', './json/proximamente.json', function() {
-        // Una vez que la animación de próximamente complete, ocultar el loader y mostrar intro
-        hideLoader();
-    });
-
+    loadLottieAnimation('full-width-lottie-loader', './json/proximamente.json');
     loadLottieAnimation('full-width-lottie-intro', './json/Intro.json');
     loadLottieAnimation('full-width-lottie', './json/pt1.json');
     loadLottieAnimation('lottie1', './json/pt2.json');
@@ -17,45 +13,49 @@ document.addEventListener('DOMContentLoaded', function() {
     loadLottieAnimation('lottie9', './json/pt10.json');
 });
 
-function loadLottieAnimation(containerId, path, onComplete) {
+function loadLottieAnimation(containerId, path) {
     var animationContainer = document.getElementById(containerId);
     var animData = {
         container: animationContainer, // ID del contenedor donde se reproducirá la animación
         renderer: 'svg',
-        loop: false,  // Ajustado para no repetir y disparar onComplete al finalizar
+        loop: true,
         autoplay: true,
         path: path // La ruta al archivo JSON de tu animación
     };
     var anim = lottie.loadAnimation(animData);
-    if (onComplete) {
-        anim.addEventListener('complete', onComplete);
-    }
 }
 
-function hideLoader() {
+document.addEventListener('DOMContentLoaded', function() {
     var loader = document.getElementById('full-width-lottie-loader');
-    var intro = document.getElementById('full-width-lottie-intro');
-
-    loader.style.opacity = 0;
-    setTimeout(function() {
-        loader.style.display = 'none';
-        intro.style.display = 'block';
-        setTimeout(function() {
-            intro.style.opacity = 1;
-        }, 10);
-    }, 500);
-}
-
-document.getElementById('full-width-lottie-intro').addEventListener('click', function() {
     var intro = document.getElementById('full-width-lottie-intro');
     var mainContent = document.getElementById('main-content');
 
-    intro.style.opacity = 0;
-    setTimeout(function() {
-        intro.style.display = 'none';
-        mainContent.style.display = 'block';
+    // Función para ocultar el loader y mostrar el intro
+    function hideLoader() {
+        loader.style.opacity = 0;
         setTimeout(function() {
-            mainContent.style.opacity = 1;
-        }, 10);
-    }, 500);
+            loader.style.display = 'none';
+            intro.style.display = 'block';
+            setTimeout(function() {
+                intro.style.opacity = 1;
+            }, 10);
+        }, 500); // Coincide con la duración de la transición de opacidad
+    }
+
+    // Simular carga y ocultar loader después de un tiempo fijo o puedes ajustarlo para cuando realmente termine de cargar todo
+    window.onload = function() {
+        hideLoader();
+    };
+
+    intro.addEventListener('click', function() {
+        // Ocultar el intro y mostrar el contenido principal
+        intro.style.opacity = 0;
+        setTimeout(function() {
+            intro.style.display = 'none';
+            mainContent.style.display = 'block';
+            setTimeout(function() {
+                mainContent.style.opacity = 1;
+            }, 10);
+        }, 500);
+    });
 });
